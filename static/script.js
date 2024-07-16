1f982f380750df87bc8b4ac5075bf44745e45579
MarieTraining <ruzmarruzmar@gmail.com>
2024-07-16 14:29:26 +0200

    added script.js to staic folder

---
+++ static/script.js
@@ -0,0 +1,10 @@
+// script.js
+function showMessage() {
+    //alert("Tohle je pouze STUDIJNÍ PROJEKT, pokračuj kliknutím");
+    var button = document.querySelector('.student-work-button');
+    if (button) {
+        button.style.display = 'none';
+    } else {
+        console.error('Button element not found.');
+    }
+}
